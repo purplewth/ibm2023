c1284e19844ca1cf8efe4d3cc5462dcce0b2b348
purplewth <61374745+purplewth@users.noreply.github.com>
2022-11-12 00:23:54 +0800

    Update solution.sql

--- Q3/solution.sql
+++ Q3/solution.sql
@@ -1,1 +1,10 @@
-
+SELECT o.owner_id, o.owner_name, COUNT(DISTINCT category_id) different_category_count
+FROM owner o
+LEFT JOIN article a
+ON o.owner_id = a.owner_id
+LEFT JOIN category_article_mapping m
+ON a.article_id = m.article_id
+LEFT JOIN category c
+ON m.category_id = c.category_id
+GROUP BY o.owner_id
+ORDER BY different_category_count desc;

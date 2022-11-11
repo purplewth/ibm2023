25976f64817e75ea06b9a9939a5af289debb9100
purplewth <61374745+purplewth@users.noreply.github.com>
2022-11-12 00:24:44 +0800

    Update solution.py

--- Q1/solution.py
+++ Q1/solution.py
@@ -1,1 +1,33 @@
+def fulfill(avail_sizes, req_sizes):
+    for size in req_sizes:
+        while size not in avail_sizes:
+            if size == 'S':
+                size = 'M'
+            elif size == 'M':
+                size = 'L'
+            elif 'S' in size:
+                size = size[1:]
+            elif 'L' in size:
+                size = 'X' + size
+            if 'L' in size and len(size)==1001:
+                return 'No'
+
+        if size in avail_sizes:
+            avail_sizes.remove(size)
+            continue
+    return 'Yes'
+                       
+
+def main():
+    N = int(input())
+    avail_sizes = input().split(' ')
+    M = int(input())
+    req_sizes = input().split(' ')
+
+    res = fulfill(avail_sizes, req_sizes)
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
 
